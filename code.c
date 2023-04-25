e8c115bcd953cf6843b3dd14e828555697d2e841
wttlab1910 <102272674+wttlab1910@users.noreply.github.com>
2023-04-25 13:27:07 +0800

    Update code.c

--- code.c
+++ code.c
@@ -1,1 +1,18 @@
 // Your C code here
+#include <stdio.h>
+
+int main(void) {
+int x,factorial =1; // define integer x
+scanf("%d", &x); // obtain integer x from user
+while(x >= 1){
+factorial = factorial * x;
+x--;
+
+}
+printf("%d", factorial);
+
+// print factorial
+
+}
+
+//task4 wtt
